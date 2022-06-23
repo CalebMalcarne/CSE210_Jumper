8dc2adb9a93c37a1d0588610b7dcd63fe8c7de22
cam christensen <93061331+CamChris98@users.noreply.github.com>
2022-06-23 17:11:19 -0600

    Update __main__.py

---
+++ Jumper/__main__.py
@@ -0,0 +1,8 @@
+#this is the main class that will call the director and start the game. 
+#main written by cam christensen, inspired by code written by caleb malcarne
+
+from game.Director import Director
+
+game = Director()
+
+game = game.start_game()
