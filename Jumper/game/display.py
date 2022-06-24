c958ddc320086a2f90ee0ce19af8f561abd85646
mikedarling12 <97472826+mikedarling12@users.noreply.github.com>
2022-06-24 13:53:38 -0600

    Update display.py

--- Jumper/game/display.py
+++ Jumper/game/display.py
@@ -1,6 +1,12 @@
-class display:
+# Written by Michael Darling.
+
+class Display:
     
     def __init__(self):
+        self.body = []
+        self.danger_floor = ""
+
+    def create_jumper(self):
         # Set up jumper man.
         self.body = []
         self.body.append("  ___  ")
@@ -10,19 +16,22 @@ class display:
         self.body.append("   0   ")
         self.body.append("  /|\  ")
         self.body.append("  / \  ")
-
         # Set up the floor under the guy, seperated
         # so that it doesn't mess up the counter.
         self.danger_floor = "^^^^^^^"
 
-
     def display_jumper(self):
         # Display the jumper.
-        for i in len(self.body):
-            print(self.body[i-1])
+        print()
+        for i in range(len(self.body)):
+            print(self.body[i])
         print()
         print(self.danger_floor)
 
     def update_jumper(self):
         # Remove body parts.
         self.body.pop(0)
+
+    def split_word(word):
+        # Split up the word and display it.
+        print(" ".join(word))
