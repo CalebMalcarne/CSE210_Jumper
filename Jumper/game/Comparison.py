838055906a395ac8a6f9d0304bab460307d3f4aa
Caleb Malcarne <caleb.malcarne@gmail.com>
2022-06-25 02:33:00 -0600

    fixed eveything

--- Jumper/game/Comparison.py
+++ Jumper/game/Comparison.py
@@ -3,11 +3,9 @@ class comparison():
 #Check if the user input matches the letter of the word
     def compare(word,user_input):
         list_of_text = list(word)
-        _user_letter = user_input
+        #_user_letter = user_input
 
-        for i in list_of_text:
-            if i == _user_letter:
-                return True
-
-            else:
-                return False
+        if user_input in word:
+            return True
+        else:
+            return False
